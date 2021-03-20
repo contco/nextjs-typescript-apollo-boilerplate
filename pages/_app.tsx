5ee5283f74dd82aed8ce62df44b82885b54e0135
Fahad Mahmood <fahad492@outlook.com>
2021-03-20 11:01:40 +0500

    apollo config added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from 'next/app';
 import { GlobalStyles } from '../styles/global';
-
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 
